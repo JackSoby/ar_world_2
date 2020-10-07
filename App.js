fd23da141b844fce979a6f6e2b6292d2d7a4d052
Jack Sobocinski <sobojack@bowst.com>
2020-10-07 14:20:44 -0400

    add init commit

--- App.js
+++ App.js
@@ -1,114 +1,90 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import React from "react";
+import { Text } from "react-native";
+import { createBottomTabNavigator, createAppContainer } from "react-navigation";
+import styles from "./assets/styles";
+import HomeScreen from "./containers/Home";
+import MatchesScreen from "./containers/Matches";
+import MessagesScreen from "./containers/Messages";
+import ProfileScreen from "./containers/Profile";
+import Icon from "./components/Icon";
 
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+const App = createBottomTabNavigator(
+	{
+		Explore: {
+			screen: HomeScreen,
+			navigationOptions: {
+				tabBarIcon: ({ focused }) => {
+					const iconFocused = focused ? "#7444C0" : "#363636";
+					return (
+						<Text style={[styles.iconMenu, { color: iconFocused }]}>
+							<Icon name="explore" />
+						</Text>
+					);
+				}
+			}
+		},
+		Matches: {
+			screen: MatchesScreen,
+			navigationOptions: {
+				tabBarIcon: ({ focused }) => {
+					const iconFocused = focused ? "#7444C0" : "#363636";
+					return (
+						<Text style={[styles.iconMenu, { color: iconFocused }]}>
+							<Icon name="heart" />
+						</Text>
+					);
+				}
+			}
+		},
+		Chat: {
+			screen: MessagesScreen,
+			navigationOptions: {
+				tabBarIcon: ({ focused }) => {
+					const iconFocused = focused ? "#7444C0" : "#363636";
+					return (
+						<Text style={[styles.iconMenu, { color: iconFocused }]}>
+							<Icon name="chat" />
+						</Text>
+					);
+				}
+			}
+		},
+		Profile: {
+			screen: ProfileScreen,
+			navigationOptions: {
+				tabBarIcon: ({ focused }) => {
+					const iconFocused = focused ? "#7444C0" : "#363636";
+					return (
+						<Text style={[styles.iconMenu, { color: iconFocused }]}>
+							<Icon name="user" />
+						</Text>
+					);
+				}
+			}
+		}
+	},
+	{
+		tabBarOptions: {
+			activeTintColor: "#7444C0",
+			inactiveTintColor: "#363636",
+			labelStyle: {
+				fontSize: 14,
+				textTransform: "uppercase",
+				paddingTop: 10
+			},
+			style: {
+				backgroundColor: "#FFF",
+				borderTopWidth: 0,
+				paddingVertical: 30,
+				height: 60,
+				marginBottom: 0,
+				shadowOpacity: 0.05,
+				shadowRadius: 10,
+				shadowColor: "#000",
+				shadowOffset: { height: 0, width: 0 }
+			}
+		}
+	}
+);
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
-
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
-
-export default App;
+export default createAppContainer(App);
